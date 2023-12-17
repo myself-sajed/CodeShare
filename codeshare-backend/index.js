b9b535b9fd9fbd772854c393524639e237f91ea3
myself-sajed <shaikhsajedcoolpop@gmail.com>
2023-12-17 21:09:07 +0530

    CodeShare by Shaikh Sajed

---
+++ codeshare-backend/index.js
@@ -0,0 +1,99 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const PORT = 5000;
+
+// map all the users
+const userSocketMap = {}
+
+// get all users from a roomID
+const getAllUsersFromRoom = (roomID) => {
+    return Array.from(io.sockets.adapter.rooms.get(roomID) || []).map(socketID => {
+        return {
+            socketID,
+            username: userSocketMap[socketID]
+        }
+    })
+}
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to CodeShare | Shaikh Sajed')
+})
+
+
+
+
+
+io.on('connection', (socket) => {
+    socket.on('join', ({ roomID, username }) => {
+
+
+        userSocketMap[socket.id] = username;
+        socket.join(roomID);
+        const allClients = getAllUsersFromRoom(roomID);
+
+        console.log('Clients are :', allClients);
+
+
+        allClients.forEach(({ socketID }) => {
+            io.to(socketID).emit('joined', {
+                allClients,
+                username,
+                socketID: socket.id
+            })
+        })
+    }
+
+    );
+
+    socket.on('disconnecting', () => {
+
+        const rooms = [...socket.rooms];
+        rooms.forEach((roomID) => {
+            socket.in(roomID).emit('disconnected', {
+                socketID: socket.id,
+                username: userSocketMap[socket.id],
+            });
+        });
+        delete userSocketMap[socket.id];
+        socket.leave();
+    })
+
+    socket.on('leavemealone', (username) => {
+        const rooms = [...socket.rooms];
+        rooms.forEach((roomID) => {
+            socket.in(roomID).emit('left', {
+                socketID: socket.id,
+                username: username,
+            });
+        });
+        delete userSocketMap[socket.id];
+        socket.leave();
+    })
+
+
+    socket.on('code-change', ({ code, roomID }) => {
+        socket.in(roomID).emit('code-change', {
+            code,
+            socketID: socket.id,
+            username: userSocketMap[socket.id],
+        })
+    })
+
+    socket.on('code-sync', ({ code, socketID }) => {
+        socket.to(socketID).emit('code-sync', {
+            code,
+        })
+    })
+});
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Listening on Port : ${PORT}`);
+});
---
+++ codeshare/src/pages/Login.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
+
+const Login = () => {
+
+    // States
+    const [roomID, setRoomID] = useState('')
+    const [username, setUsername] = useState('')
+    const navigate = useNavigate()
+
+    // Generating Room ID
+    function generateRoomId() {
+        const ID = uuid()
+        setRoomID(ID)
+        setUsername('Shaikh Sajed')
+        toast.success('Room ID generated successfully!')
+    }
+
+    // Creating Room 
+    function createRoom() {
+        if (roomID === '' || username === '') {
+            toast.error('All fields are required!')
+        } else {
+            navigate(`/editor/${roomID}`, { state: { username: username, roomID: roomID } })
+            toast.success('Room created successfully!')
+        }
+    }
+
+    function checkEnter(event) {
+        if (event.key === 'Enter') {
+            createRoom()
+        }
+    }
+
+
+    return (
+        <div className="p-3 pl-20 h-screen bg-blue-200">
+            <img src="/assets/logo.png" draggable="false" className="h-36" alt="logo" />
+            <div className='max-h-screen flex align-start justify-start gap-20 mx-auto'>
+                <div>
+                    <p className='text-5xl ml-10 font-bold text-blue-500'>The best platform for <br />
+                        <span className='text-blue-800 ml-2'>Realtime Coding</span></p>
+                    <img src="/assets/hero.svg" draggable="false" className='h-96 responsive' alt="" />
+                </div>
+                <div className='bg-blue-100 rounded-[20px] p-3 w-[500px] '>
+                    <img src="/assets/logo.png" draggable="false" className='h-36 mx-auto' alt="logo" />
+                    <div className='flex flex-col mx-10 gap-3'>
+
+                        <p className='text-blue-500 text-sm my-0'>Paste Invitation Room ID here</p>
+
+                        {/* INPUTS */}
+
+                        {/* 1. ROOM ID */}
+                        <input type="text" value={roomID} onChange={(e) => { setRoomID(e.target.value) }} placeholder="Enter or paste Room ID" className="p-3 text-lg bg-transparent outline-none border border-blue-400 rounded-full" onKeyUp={checkEnter} />
+
+                        {/* 2. USERNAME */}
+                        <input type="text" value={username} onChange={(e) => { setUsername(e.target.value) }} placeholder="Enter Username" className="p-3 text-lg bg-transparent outline-none border border-blue-400 rounded-full" onKeyUp={checkEnter} />
+
+                        <button className='p-3 bg-blue-600 text-white rounded-full hover:bg-blue-800 ease-in-out duration-300 text-lg' onClick={createRoom}>Join Room</button>
+                    </div>
+                    <p className='text-center mt-10'>If you don't have an invite, <span className='text-blue-600 cursor-pointer hover:text-blue-900 font-bold ease-in-out duration-300' onClick={generateRoomId}>Create a Room</span></p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
